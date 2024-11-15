610e372bbea5fb6a68b4faa0b11badde9850eb9d
rubensiregar <rubbento88@gmail.com>
2024-11-15 11:15:01 +0700

    effi gambar 2

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,8 +4,15 @@ import * as React from "react";
 const HeroSection: React.FunctionComponent = () => {
     return (
         <section id="hero" className="h-svh bg-violet-200 relative">
-            <Image src="/img/hero.webp" alt="hero" objectFit="cover" fill />
-s        </section>
+            <Image
+                quality={40}
+                src="/img/herooo.webp"
+                alt="hero"
+                objectFit="cover"
+                fill
+            />
+            s{" "}
+        </section>
     );
 };
 

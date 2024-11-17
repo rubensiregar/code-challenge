b84cefd829cebf67cb46d52fe37df6ced39fe89c
rubensiregar <rubbento88@gmail.com>
2024-11-17 18:50:53 +0700

    contentful 2

--- src/lib/contentfulClient.ts
+++ src/lib/contentfulClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "contentful";
 
 const contentfulClient = createClient({
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || "",
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || "",
+    space: process.env.NEXT_CONTENTFUL_SPACE_ID || "",
+    accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN || "",
 });
 
 export default contentfulClient;
